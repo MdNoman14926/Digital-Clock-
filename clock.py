d90f9e608ce995d7406c488b394ef97306bbc518
Code With Noman <85833961+MdNoman14926@users.noreply.github.com>
2023-02-26 12:58:39 +0600

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,14 @@
+import time
+from tkinter import*
+root=Tk()
+root.configure(background="Black")
+
+def start():
+    text = time.strftime("%H:%M:%S")
+    Label.config(text=text)
+    Label.after(200,start)
+Label = Label(root,font=("Ds-Digital",100),fg="red",bg="black")
+Label.grid(row=0,column=1)
+print("Done")
+start()
+root.mainloop()
